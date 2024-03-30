fce20ac5f3401c2e665ad0519667efe7cb88f525
Edmar Junyor Bevilaqua <edmar.bevi@gmail.com>
2024-03-30 15:56:04 -0300

    Create class Produto and enum Categoria

---
+++ springboot/src/main/java/br/com/springboot/model/Produto.java
@@ -0,0 +1,68 @@
+package br.com.springboot.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(nullable = false, length = 50)
+	@NotBlank(message = "Informe um nome para o produto!")
+	private String nome;
+	
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private Categoria categoria;
+	
+	private boolean ativo;
+	
+	public Produto() {
+		this.ativo = true;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
+}
