933691b19464550af41d5680b7c1965970607e94
Edmar Junyor Bevilaqua <edmar.bevi@gmail.com>
2024-04-01 15:11:10 -0300

    Create NotaEntrada.java
    Created the first version of NotaEntrada

---
+++ springboot/src/main/java/br/com/springboot/model/NotaEntrada.java
@@ -0,0 +1,69 @@
+package br.com.springboot.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table(name = "notas_entrada")
+public class NotaEntrada {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@DateTimeFormat(iso = ISO.DATE_TIME)
+	@Column(nullable = false, name="data_hora", columnDefinition = "DATETIME")
+	private LocalDateTime dataHora;
+	
+	@Transient
+	private Float total;
+	
+	@ManyToOne
+	@JoinColumn(name = "fornecedor_id", nullable = false)
+	private Fornecedor fornecedor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+
+	public Float getTotal() {
+		return total;
+	}
+
+	public void setTotal(Float total) {
+		this.total = total;
+	}
+
+	public Fornecedor getFornecedor() {
+		return fornecedor;
+	}
+
+	public void setFornecedor(Fornecedor fornecedor) {
+		this.fornecedor = fornecedor;
+	}
+	
+}
